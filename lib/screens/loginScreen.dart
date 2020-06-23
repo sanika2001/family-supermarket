64897a23dab985def2bb9057cf3d03c7cf31323a
jyothir10 <j.adithyan16@gmail.com>
2020-06-23 21:26:22 +0530

    loginScreen.dart

--- lib/screens/loginScreen.dart
+++ lib/screens/loginScreen.dart
@@ -10,7 +10,23 @@ class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+      body: Column(
+        children: <Widget>[
+          Center(
+            child: Card(
+              elevation: 4,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(20),
+              ),
+              child: Column(
+                children: <Widget>[
+                  Image(image: AssetImage(""),),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
