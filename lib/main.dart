9e495df7d6ac6c33be4cd762d4b08555b3aa7e2a
jyothir10 <j.adithyan16@gmail.com>
2020-06-25 23:19:05 +0530

    loginScreen.dart

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      initialRoute: SupermarketScreen.id,
+      initialRoute: LoginScreen.id,
       routes: {
         SupermarketScreen.id: (context) => SupermarketScreen(),
         LoginScreen.id: (context) => LoginScreen()
--- lib/screens/loginScreen.dart
+++ lib/screens/loginScreen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class LoginScreen extends StatefulWidget {
@@ -11,18 +12,136 @@ class _LoginScreenState extends State<LoginScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
-          Center(
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 10),
             child: Card(
-              elevation: 4,
+              elevation: 10,
               shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(20),
+                borderRadius: BorderRadius.circular(10),
               ),
               child: Column(
                 children: <Widget>[
-                  Image(image: AssetImage(""),),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Image(
+                      image: AssetImage("images/familylogo.png"),
+                      height: 60,
+                      width: 60,
+                    ),
+                  ),
+                  Padding(
+                    padding:
+                        const EdgeInsets.symmetric(horizontal: 25, vertical: 8),
+                    child: TextField(
+                      textAlign: TextAlign.start,
+                      decoration: InputDecoration(
+                        hintText: 'Mobile No',
+                        contentPadding:
+                            EdgeInsets.symmetric(vertical: 2, horizontal: 20),
+                        enabledBorder: OutlineInputBorder(
+                          borderSide:
+                              BorderSide(color: Color(0xFFC4C4C4), width: 1.0),
+                          borderRadius: BorderRadius.all(Radius.circular(5)),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderSide:
+                              BorderSide(color: Color(0xFFC4C4C4), width: 1.0),
+                          borderRadius: BorderRadius.all(Radius.circular(5)),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding:
+                        const EdgeInsets.symmetric(horizontal: 25, vertical: 8),
+                    child: TextField(
+                      textAlign: TextAlign.start,
+                      decoration: InputDecoration(
+                        hintText: 'Password',
+                        contentPadding:
+                            EdgeInsets.symmetric(vertical: 2, horizontal: 20),
+                        enabledBorder: OutlineInputBorder(
+                          borderSide:
+                              BorderSide(color: Color(0xFFC4C4C4), width: 1.0),
+                          borderRadius: BorderRadius.all(Radius.circular(5)),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderSide:
+                              BorderSide(color: Color(0xFFC4C4C4), width: 1.0),
+                          borderRadius: BorderRadius.all(Radius.circular(5)),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: <Widget>[
+                      Padding(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 25, vertical: 8),
+                        child: Text(
+                          "Forgot Password?",
+                          style: TextStyle(
+                            decoration: TextDecoration.underline,
+                            color: Color(0xFF939393),
+                            fontSize: 15,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Padding(
+                    padding:
+                        const EdgeInsets.only(left: 25, right: 25, bottom: 25),
+                    child: ButtonTheme(
+                      minWidth: 150,
+                      child: RaisedButton(
+                        color: Color(0xFF740F53),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(5),
+                        ),
+                        child: Text(
+                          "Sign in",
+                        ),
+                        textColor: Colors.white,
+                        onPressed: () {},
+                      ),
+                    ),
+                  ),
                 ],
               ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  "Don't have account?",
+                  style: TextStyle(
+                    decoration: TextDecoration.underline,
+                    color: Color(0xFF939393),
+                  ),
+                ),
+                Text(
+                  " Sign up",
+                  style: TextStyle(
+                    color: Color(0xFF741053),
+                    fontWeight: FontWeight.bold,
+                    fontSize: 15,
+                  ),
+                )
+              ],
+            ),
+          ),
+          Text(
+            "Terms & Conditions",
+            textAlign: TextAlign.end,
+            style: TextStyle(
+              color: Color(0xFF939393),
             ),
           ),
         ],
