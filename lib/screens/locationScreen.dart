685726e85010b228bef49a2990b8ec74c8aa16f9
jyothir10 <j.adithyan16@gmail.com>
2020-06-28 12:51:44 +0530

    locationScreen.dart

--- lib/screens/locationScreen.dart
+++ lib/screens/locationScreen.dart
@@ -12,12 +12,71 @@ class _locationScreenState extends State<locationScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Map"),
+        leading: Icon(
+          Icons.arrow_back,
+          color: Color(0xFF464646),
+          size: 30,
+        ),
+        backgroundColor: Color(0xFFE9E9E9),
+        title: Text(
+          "NEAREST  BRANCH",
+          style: TextStyle(
+            fontSize: 22,
+            color: Color(0xFF740F53),
+            fontWeight: FontWeight.bold,
+          ),
+        ),
       ),
-      body: GoogleMap(
-        initialCameraPosition:
-            CameraPosition(target: LatLng(27.2038, 77.5011), zoom: 15),
+      bottomNavigationBar: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+              child: ButtonTheme(
+                minWidth: 175,
+                child: RaisedButton(
+                  color: Color(0xFF740F53),
+                  onPressed: () {},
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Text(
+                    "SET LOCATION",
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+      body: Stack(
+        children: <Widget>[
+          GoogleMap(
+            initialCameraPosition:
+                CameraPosition(target: LatLng(27.2038, 77.5011), zoom: 15),
+          ),
+        ],
       ),
     );
   }
 }
+
+//Container(
+//alignment: Alignment.bottomCenter,
+//child: ButtonTheme(
+//minWidth: 160,
+//child: RaisedButton(
+//onPressed: null,
+//color: Color(0xFF740F53),
+//shape: RoundedRectangleBorder(
+//borderRadius: BorderRadius.circular(5),
+//),
+//child: Text("SET LOCATION"),
+//textColor: Colors.white,
+//),
+//),
+//),
