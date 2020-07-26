412159286f82d04a70b00c2fb06f3ffc56b1e02f
jyothir10 <j.adithyan16@gmail.com>
2020-07-26 20:49:25 +0530

    cartScreen.dart

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      initialRoute: homeScreen.id,
+      initialRoute: CartScreen.id,
       routes: {
         SupermarketScreen.id: (context) => SupermarketScreen(),
         LoginScreen.id: (context) => LoginScreen(),
