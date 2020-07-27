71263412b3809409d37a49677d1336aad9740bad
sanika2001 <porayilsanika@gmail.com>
2020-07-27 12:15:56 +0530

    welcome screen

--- lib/screens/WelcomeScreen.dart
+++ lib/screens/WelcomeScreen.dart
@@ -10,14 +10,15 @@ class WelcomeScreen extends StatefulWidget {
 class _WelcomeScreenState extends State<WelcomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        Stack(
-          children: <Widget>[
-            Container(
-              color: Colors.white,
-              height: 280,
-            ),
+    return Scaffold(
+      body: Column(
+        children: <Widget>[
+          Stack(
+            children: <Widget>[
+              Container(
+                color: Colors.white,
+                height: 280,
+              ),
 //        ClipPath(
 //          child: Container(
 //            width: MediaQuery.of(context).size.width,
@@ -28,104 +29,105 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
 //          ),
 //          clipper: BottomWaveClipper(),
 //        ),
-            ClipPath(
-              clipper: ClippingClass(),
-              child: Container(
-                width: MediaQuery.of(context).size.width,
-                height: 280.0,
-                decoration: BoxDecoration(
-                  gradient: LinearGradient(
-                    begin: FractionalOffset.topCenter,
-                    end: FractionalOffset.bottomCenter,
-                    colors: [
-                      kDesignColor,
-                      Colors.white,
-                    ],
-                    stops: [0.0, 1],
+              ClipPath(
+                clipper: ClippingClass(),
+                child: Container(
+                  width: MediaQuery.of(context).size.width,
+                  height: 280.0,
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                      begin: FractionalOffset.topCenter,
+                      end: FractionalOffset.bottomCenter,
+                      colors: [
+                        kDesignColor,
+                        Colors.white,
+                      ],
+                      stops: [0.0, 1],
+                    ),
                   ),
                 ),
               ),
-            ),
-          ],
-        ),
-        Container(
-          height: MediaQuery.of(context).size.height - 280,
-          width: MediaQuery.of(context).size.width,
-          color: Colors.white,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: <Widget>[
-              Image(
-                image: AssetImage("images/logo.png"),
-                height: 130,
-              ),
-              Column(
-                children: <Widget>[
-                  Text(
-                    "Welcome To",
-                    style: TextStyle(
-                      color: Color(0xFF740F53),
-                      fontSize: 20,
-                    ),
-                  ),
-                  Text(
-                    "FAMILY SUPERMARKET",
-                    style: TextStyle(
-                      color: Color(0xFF740F53),
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ],
-              ),
-              Column(
-                children: <Widget>[
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Card(
-                      color: Color(0xFF740F53),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10),
+            ],
+          ),
+          Container(
+            height: MediaQuery.of(context).size.height - 280,
+            width: MediaQuery.of(context).size.width,
+            color: Colors.white,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                Image(
+                  image: AssetImage("images/logo.png"),
+                  height: 130,
+                ),
+                Column(
+                  children: <Widget>[
+                    Text(
+                      "Welcome To",
+                      style: TextStyle(
+                        color: Color(0xFF740F53),
+                        fontSize: 20,
                       ),
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(
-                            horizontal: 35, vertical: 5),
-                        child: Text(
-                          "Sign in",
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 17,
+                    ),
+                    Text(
+                      "FAMILY SUPERMARKET",
+                      style: TextStyle(
+                        color: Color(0xFF740F53),
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+                Column(
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Card(
+                        color: Color(0xFF740F53),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 35, vertical: 5),
+                          child: Text(
+                            "Sign In",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 17,
+                            ),
                           ),
                         ),
                       ),
                     ),
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      Text(
-                        "New customer?",
-                        style: TextStyle(
-                          fontSize: 15,
-                          color: Color(0xFF740F53),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Text(
+                          "New customer?",
+                          style: TextStyle(
+                              fontSize: 16,
+                              color: Color(0xFF740F53),
+                              fontWeight: FontWeight.w500),
                         ),
-                      ),
-                      Text(
-                        "Sign Up",
-                        style: TextStyle(
-                          decoration: TextDecoration.underline,
-                          fontSize: 15,
-                          color: Colors.black,
+                        Text(
+                          "Sign Up",
+                          style: TextStyle(
+                              decoration: TextDecoration.underline,
+                              fontSize: 16,
+                              color: Colors.black,
+                              fontWeight: FontWeight.w500),
                         ),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-            ],
+                      ],
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
