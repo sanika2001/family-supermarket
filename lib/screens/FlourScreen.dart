8da354af25384d7a83a1ab7c9e1c60096f15fed7
sanika2001 <porayilsanika@gmail.com>
2020-07-27 22:01:31 +0530

    home screen

--- lib/screens/FlourScreen.dart
+++ lib/screens/FlourScreen.dart
@@ -48,12 +48,134 @@ class _FlourScreenState extends State<FlourScreen> {
                               snapshot.data[index].pressed = true;
                             });
                           },
+                          plus: () {
+                            setState(() {
+                              snapshot.data[index].qty++;
+                            });
+                          },
+                          minus: () {
+                            setState(() {
+                              if (snapshot.data[index].qty > 0) {
+                                snapshot.data[index].qty--;
+                              }
+                            });
+                          },
+                          popup: () {
+                            showModalBottomSheet(
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.only(
+                                  topLeft: Radius.circular(20),
+                                  topRight: Radius.circular(20),
+                                ),
+                              ),
+                              context: context,
+                              builder: (BuildContext bc) {
+                                return BottomSheet();
+                              },
+                            );
+                          },
                         );
                       })
                   : Center(
                       child: CircularProgressIndicator(),
                     );
             }),
+      ),
+    );
+  }
+}
+
+class BottomSheet extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(20),
+          topRight: Radius.circular(20),
+        ),
+      ),
+      height: 300,
+      child: Column(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.only(left: 20, top: 20, bottom: 10),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                  "Available Quantities",
+                  style: TextStyle(
+                    color: Color(0xFF464646),
+                    fontWeight: FontWeight.bold,
+                    fontSize: 19,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Divider(
+            color: Color(0xFFC4C4C4),
+            thickness: 3,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Text(
+                  "1 kg",
+                  style: TextStyle(
+                    color: Color(0xFF464646),
+                    fontWeight: FontWeight.bold,
+                    fontSize: 19,
+                  ),
+                ),
+                Text(
+                  "₹ 32",
+                  style: TextStyle(
+                    color: Color(0xFF464646),
+                    fontWeight: FontWeight.bold,
+                    fontSize: 19,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Divider(
+            color: Color(0xFFC4C4C4),
+            thickness: 3,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Text(
+                  "2 kg",
+                  style: TextStyle(
+                    color: Color(0xFF464646),
+                    fontWeight: FontWeight.bold,
+                    fontSize: 19,
+                  ),
+                ),
+                Text(
+                  "₹ 60",
+                  style: TextStyle(
+                    color: Color(0xFF464646),
+                    fontWeight: FontWeight.bold,
+                    fontSize: 19,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Divider(
+            color: Color(0xFFC4C4C4),
+            thickness: 3,
+          ),
+        ],
       ),
     );
   }
