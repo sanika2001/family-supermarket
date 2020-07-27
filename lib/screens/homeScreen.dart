c24c35f51c2a8dd11f3d8b90d4f385a6f45acdd4
sanika2001 <porayilsanika@gmail.com>
2020-07-27 11:25:56 +0530

    Tab bar

--- lib/screens/homeScreen.dart
+++ lib/screens/homeScreen.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:familysupermarket/bloc/homeBloc.dart';
 import 'package:familysupermarket/models/home.dart';
 import 'package:familysupermarket/components/homeCard.dart';
-import 'package:cupertino_tabbar/cupertino_tabbar.dart' as CupertinoTabBar;
+import 'package:familysupermarket/constants.dart';
 import 'package:familysupermarket/bloc/RiceBloc.dart';
 import 'package:familysupermarket/components/riceCard.dart';
 import 'package:familysupermarket/models/rice.dart';
@@ -25,156 +25,161 @@ class _homeScreenState extends State<homeScreen> {
   @override
   void dispose() {
     _homeBloc.dispose();
-    _riceBloc.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        elevation: 5,
-        backgroundColor: Colors.white,
-        title: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Image(
-              image: AssetImage("images/logo.png"),
-              height: 60,
-              width: 60,
-            ),
-            Text(
-              "FAMILY",
-              style: TextStyle(
-                color: Color(0xFF740F53),
-                fontWeight: FontWeight.bold,
-                fontSize: 24,
+    return DefaultTabController(
+      length: 4,
+      child: Scaffold(
+        appBar: AppBar(
+          elevation: 5,
+          backgroundColor: Colors.white,
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Image(
+                image: AssetImage("images/logo.png"),
+                height: 60,
+                width: 60,
               ),
-            ),
-          ],
+              Text(
+                "FAMILY",
+                style: TextStyle(
+                  color: Color(0xFF740F53),
+                  fontWeight: FontWeight.bold,
+                  fontSize: 24,
+                ),
+              ),
+            ],
+          ),
         ),
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: <Widget>[
-            Container(
-              height: 128,
-              color: Color(0xFFE9E9E9),
-              child: StreamBuilder<List<Home>>(
-                  stream: _homeBloc.homeListStream,
-                  builder: (BuildContext context,
-                      AsyncSnapshot<List<Home>> snapshot) {
-                    if (snapshot.hasError) {
-                      print(snapshot.error);
-                    }
-                    return snapshot.hasData
-                        ? ListView.builder(
-                            physics: BouncingScrollPhysics(),
-                            itemCount: snapshot.data.length,
-                            scrollDirection: Axis.horizontal,
-                            itemBuilder: (context, index) {
-                              return HomeCard(
-                                home: snapshot.data[index],
-                              );
-                            })
-                        : Center(
-                            child: CircularProgressIndicator(),
-                          );
-                  }),
-            ),
-            CupertinoTabBar.CupertinoTabBar(
-              Color(0xFF740F53),
-              Color(0xFFC4C4C4),
-              [
-                Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 8),
-                  child: Text(
-                    "Rice",
-                    style: const TextStyle(
-                      color: Colors.white,
-                      fontSize: 15.0,
-                      fontWeight: FontWeight.bold,
-                    ),
-                    textAlign: TextAlign.center,
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 8),
-                  child: Text(
-                    "Flour & Atta",
-                    style: const TextStyle(
-                      color: Colors.white,
-                      fontSize: 15.0,
-                      fontWeight: FontWeight.bold,
-                    ),
-                    textAlign: TextAlign.center,
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 8),
-                  child: Text(
-                    "Dal & Pulses",
-                    style: const TextStyle(
-                      color: Colors.white,
-                      fontSize: 15,
-                      fontWeight: FontWeight.bold,
-                    ),
-                    textAlign: TextAlign.center,
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 8),
-                  child: Text(
-                    "Spices & Masala",
-                    style: const TextStyle(
-                      color: Colors.white,
-                      fontSize: 14,
-                      fontWeight: FontWeight.bold,
-                    ),
-                    textAlign: TextAlign.center,
-                  ),
-                ),
-              ],
-              cupertinoTabBarValueGetter,
-              (int index) {
-                setState(() {
-                  cupertinoTabBarValue = index;
-                  print(cupertinoTabBarValue);
-                });
-              },
-              horizontalPadding: 0,
-              verticalPadding: 10,
-              useSeparators: false,
-              borderRadius: BorderRadius.all(
-                Radius.circular(0),
+        body: SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+              Container(
+                height: 128,
+                color: Color(0xFFE9E9E9),
+                child: StreamBuilder<List<Home>>(
+                    stream: _homeBloc.homeListStream,
+                    builder: (BuildContext context,
+                        AsyncSnapshot<List<Home>> snapshot) {
+                      if (snapshot.hasError) {
+                        print(snapshot.error);
+                      }
+                      return snapshot.hasData
+                          ? ListView.builder(
+                              physics: BouncingScrollPhysics(),
+                              itemCount: snapshot.data.length,
+                              scrollDirection: Axis.horizontal,
+                              itemBuilder: (context, index) {
+                                return HomeCard(
+                                  home: snapshot.data[index],
+                                );
+                              })
+                          : Center(
+                              child: CircularProgressIndicator(),
+                            );
+                    }),
               ),
-            ),
-            Container(
-              height: 500,
-              child: StreamBuilder<List<Rice>>(
-                  stream: _riceBloc.riceListStream,
-                  builder: (BuildContext context,
-                      AsyncSnapshot<List<Rice>> snapshot) {
-                    if (snapshot.hasError) {
-                      print(snapshot.error);
-                    }
-                    return snapshot.hasData
-                        ? ListView.builder(
-                            physics: BouncingScrollPhysics(),
-                            itemCount: snapshot.data.length,
-                            scrollDirection: Axis.vertical,
-                            itemBuilder: (context, index) {
-                              return RiceCard(
-                                rice: snapshot.data[index],
-                              );
-                            })
-                        : Center(
-                            child: CircularProgressIndicator(),
-                          );
-                  }),
-            ),
-          ],
+              Container(
+                color: kDesignColor,
+                height: 50,
+                width: MediaQuery.of(context).size.width,
+                child: TabBar(
+                    indicatorColor: kDesignColor,
+                    isScrollable: true,
+                    labelStyle: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.w400,
+                        fontSize: 16),
+                    tabs: [
+                      Tab(
+                        text: "Rice",
+                      ),
+                      Tab(
+                        text: "Flour&Atta",
+                      ),
+                      Tab(
+                        text: "Dal&Pulses",
+                      ),
+                      Tab(
+                        text: "Spices&Masala",
+                      ),
+                    ]),
+              ),
+              Container(
+                height: MediaQuery.of(context).size.height - 130,
+                width: MediaQuery.of(context).size.width,
+                child: TabBarView(
+                  children: <Widget>[
+                    SingleChildScrollView(
+                      child: Container(
+                        height: 500,
+                        child: StreamBuilder<List<Rice>>(
+                            stream: _riceBloc.riceListStream,
+                            builder: (BuildContext context,
+                                AsyncSnapshot<List<Rice>> snapshot) {
+                              if (snapshot.hasError) {
+                                print(snapshot.error);
+                              }
+                              return snapshot.hasData
+                                  ? ListView.builder(
+                                      physics: BouncingScrollPhysics(),
+                                      itemCount: snapshot.data.length,
+                                      scrollDirection: Axis.vertical,
+                                      itemBuilder: (context, index) {
+                                        return RiceCard(
+                                          rice: snapshot.data[index],
+                                        );
+                                      })
+                                  : Center(
+                                      child: CircularProgressIndicator(),
+                                    );
+                            }),
+                      ),
+                    ),
+                    Container(
+                      child: Text("Atta&Flour"),
+                    ),
+                    Container(
+                      child: Text("Dal&Pulses"),
+                    ),
+                    Container(
+                      child: Text("Spices&Masala"),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
   }
 }
+
+//Container(
+//height: 500,
+//child: StreamBuilder<List<Rice>>(
+//stream: _riceBloc.riceListStream,
+//builder: (BuildContext context,
+//    AsyncSnapshot<List<Rice>> snapshot) {
+//if (snapshot.hasError) {
+//print(snapshot.error);
+//}
+//return snapshot.hasData
+//? ListView.builder(
+//physics: BouncingScrollPhysics(),
+//itemCount: snapshot.data.length,
+//scrollDirection: Axis.vertical,
+//itemBuilder: (context, index) {
+//return RiceCard(
+//rice: snapshot.data[index],
+//);
+//})
+//    : Center(
+//child: CircularProgressIndicator(),
+//);
+//}),
+//),
