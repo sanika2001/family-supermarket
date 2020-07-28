ac0de36f83c04c20ebb4526e79c80436a1a976a8
sanika2001 <porayilsanika@gmail.com>
2020-07-28 14:15:19 +0530

    profile screen

--- lib/screens/profileScreen.dart
+++ lib/screens/profileScreen.dart
@@ -24,114 +24,130 @@ class _ProfileSCreenState extends State<ProfileSCreen> {
           ),
         ],
       ),
-      body: Stack(
-        children: <Widget>[
-          ClipPath(
-            clipper: ClippingClass(),
-            child: Container(
-              width: MediaQuery.of(context).size.width,
-              height: 300.0,
-              color: Color(0xFFA63F85),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  CircleAvatar(
-                    backgroundImage: AssetImage("images/two.jpg"),
-                    radius: 60,
-                    backgroundColor: Colors.white,
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(bottom: 70,top: 10),
-                    child: Text(
-                      "Aleena Thomas",
-                      style: TextStyle(
-                        color: Color(0xFFE9E9E9),
-                        fontSize: 18,
-                        fontWeight: FontWeight.bold,
+      body: SingleChildScrollView(
+        child: Stack(
+          children: <Widget>[
+            Container(
+              height: MediaQuery.of(context).size.height,
+            ),
+            ClipPath(
+              clipper: ClippingClass(),
+              child: Container(
+                width: MediaQuery.of(context).size.width,
+                height: 300.0,
+                color: Color(0xFFA63F85),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    CircleAvatar(
+                      backgroundImage: AssetImage("images/two.jpg"),
+                      radius: 60,
+                      backgroundColor: Colors.white,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(bottom: 70, top: 10),
+                      child: Text(
+                        "Aleena Thomas",
+                        style: TextStyle(
+                          color: Color(0xFFE9E9E9),
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                        ),
                       ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
             ),
-          ),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: <Widget>[
-              Padding(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 30, vertical: 15),
-                child: Card(
-                  elevation: 15,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Column(
-                      children: <Widget>[
-                        familyCard(name: "Contact details"),
-                        Padding(
-                          padding: const EdgeInsets.symmetric(horizontal: 8),
-                          child: Divider(
-                            color: Color(0xFFC4C4C4),
+            Positioned(
+              top: 200,
+              child: Container(
+                width: MediaQuery.of(context).size.width,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 30, vertical: 15),
+                      child: Card(
+                        elevation: 15,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Column(
+                            children: <Widget>[
+                              familyCard(name: "Contact details"),
+                              Padding(
+                                padding:
+                                    const EdgeInsets.symmetric(horizontal: 8),
+                                child: Divider(
+                                  color: Color(0xFFC4C4C4),
+                                ),
+                              ),
+                              familyCard(name: "Delivery Address"),
+                            ],
                           ),
                         ),
-                        familyCard(name: "Delivery Address"),
-                      ],
+                      ),
                     ),
-                  ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 30, vertical: 20),
+                      child: Card(
+                        elevation: 15,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Column(
+                            children: <Widget>[
+                              familyCard(name: "My Orders"),
+                              Padding(
+                                padding:
+                                    const EdgeInsets.symmetric(horizontal: 8),
+                                child: Divider(
+                                  color: Color(0xFFC4C4C4),
+                                ),
+                              ),
+                              familyCard(name: "My Cards & Wallets"),
+                              Padding(
+                                padding:
+                                    const EdgeInsets.symmetric(horizontal: 8),
+                                child: Divider(
+                                  color: Color(0xFFC4C4C4),
+                                ),
+                              ),
+                              familyCard(name: "Coupons & Points"),
+                              Padding(
+                                padding:
+                                    const EdgeInsets.symmetric(horizontal: 8),
+                                child: Divider(
+                                  color: Color(0xFFC4C4C4),
+                                ),
+                              ),
+                              familyCard(name: "Wishlist"),
+                              Padding(
+                                padding:
+                                    const EdgeInsets.symmetric(horizontal: 8),
+                                child: Divider(
+                                  color: Color(0xFFC4C4C4),
+                                ),
+                              ),
+                              familyCard(name: "Customer Care")
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
               ),
-              Padding(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 30, vertical: 20),
-                child: Card(
-                  elevation: 15,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Column(
-                      children: <Widget>[
-                        familyCard(name: "My Orders"),
-                        Padding(
-                          padding: const EdgeInsets.symmetric(horizontal: 8),
-                          child: Divider(
-                            color: Color(0xFFC4C4C4),
-                          ),
-                        ),
-                        familyCard(name: "My Cards & Wallets"),
-                        Padding(
-                          padding: const EdgeInsets.symmetric(horizontal: 8),
-                          child: Divider(
-                            color: Color(0xFFC4C4C4),
-                          ),
-                        ),
-                        familyCard(name: "Coupons & Points"),
-                        Padding(
-                          padding: const EdgeInsets.symmetric(horizontal: 8),
-                          child: Divider(
-                            color: Color(0xFFC4C4C4),
-                          ),
-                        ),
-                        familyCard(name: "Wishlist"),
-                        Padding(
-                          padding: const EdgeInsets.symmetric(horizontal: 8),
-                          child: Divider(
-                            color: Color(0xFFC4C4C4),
-                          ),
-                        ),
-                        familyCard(name: "Customer Care")
-                      ],
-                    ),
-                  ),
-                ),
-              ),
-            ],
-          ),
-        ],
+            ),
+          ],
+        ),
       ),
     );
   }
