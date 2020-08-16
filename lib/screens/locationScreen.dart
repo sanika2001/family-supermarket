d7fe4bfae8b5c08a47c83c51cd45271d2072d8a4
sanika2001 <porayilsanika@gmail.com>
2020-08-16 23:52:06 +0530

    map

--- lib/screens/locationScreen.dart
+++ lib/screens/locationScreen.dart
@@ -34,8 +34,7 @@ class _locationScreenState extends State<locationScreen> {
   GoogleMapController _mapController;
 
   void _onMapCreated(GoogleMapController controller) {
-    print("called");
-    print(latitude);
+
     _mapController = controller;
     setState(() {
       _markers.add(Marker(
