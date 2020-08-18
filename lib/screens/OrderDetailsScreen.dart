fe00f7adef66dc422378f30e9d52559b0213654e
sanika2001 <porayilsanika@gmail.com>
2020-08-18 22:54:11 +0530

    order details

--- lib/screens/OrderDetailsScreen.dart
+++ lib/screens/OrderDetailsScreen.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:familysupermarket/constants.dart';
 import 'package:step_progress_indicator/step_progress_indicator.dart';
 import 'package:familysupermarket/components/DeliveryStatus.dart';
+import 'package:smooth_star_rating/smooth_star_rating.dart';
 
 class OrderDetailsScreen extends StatefulWidget {
   static const String id = '/orderdetails';
@@ -14,6 +15,7 @@ class OrderDetailsScreen extends StatefulWidget {
 class _OrderDetailsScreenState extends State<OrderDetailsScreen> {
   @override
   Widget build(BuildContext context) {
+    double rating = 0;
     return Scaffold(
       appBar: AppBar(
         title: Text("Order Details"),
@@ -31,123 +33,194 @@ class _OrderDetailsScreenState extends State<OrderDetailsScreen> {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.center,
             children: <Widget>[
-              Container(
-                child: Padding(
-                  padding:
-                      const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
-                  child: Card(
-                    elevation: 4,
-                    color: Colors.white,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(20)),
-                    ),
-                    child: Column(
-                      children: <Widget>[
-                        Padding(
-                          padding: const EdgeInsets.all(12.0),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: <Widget>[
-                              Column(
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+                child: Card(
+                  elevation: 4,
+                  color: Colors.white,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(20)),
+                  ),
+                  child: Column(
+                    children: <Widget>[
+                      Padding(
+                        padding: const EdgeInsets.all(12.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: <Widget>[
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: <Widget>[
+                                Padding(
+                                  padding: const EdgeInsets.all(5.0),
+                                  child: Text(
+                                    "Periyar Rice",
+                                    style: TextStyle(
+                                        fontSize: 20,
+                                        fontWeight: FontWeight.w400),
+                                  ),
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.all(5.0),
+                                  child: Text(
+                                    "seller: Seller name",
+                                    style: TextStyle(
+                                      fontSize: 16,
+                                    ),
+                                  ),
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: Text(
+                                    "₹ 32.00",
+                                    style: TextStyle(
+                                        color: Colors.black,
+                                        fontSize: 22,
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                )
+                              ],
+                            ),
+                            CachedNetworkImage(
+                              imageUrl:
+                                  "https://www.periyarrice.com/images/slider2_pro1.png",
+                              height: 120,
+                              width: 120,
+                            ),
+                          ],
+                        ),
+                      ),
+                      Divider(
+                        color: Colors.grey,
+                        thickness: 1,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(
+                            vertical: 5, horizontal: 25),
+                        child: Row(
+                          children: <Widget>[
+                            Container(
+                              height: 200,
+                              child: StepProgressIndicator(
+                                totalSteps: 4,
+                                direction: Axis.vertical,
+                                currentStep: 3,
+                                selectedColor: kDesignColor,
+                                unselectedColor: Colors.grey,
+                                size: 8,
+                                padding: 1,
+                                roundedEdges: Radius.circular(10),
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.only(left: 15),
+                              child: Column(
                                 crossAxisAlignment: CrossAxisAlignment.start,
                                 children: <Widget>[
-                                  Padding(
-                                    padding: const EdgeInsets.all(5.0),
-                                    child: Text(
-                                      "Periyar Rice",
-                                      style: TextStyle(
-                                          fontSize: 20,
-                                          fontWeight: FontWeight.w400),
-                                    ),
+                                  DeliveryStatus(
+                                    text: "Ordered",
+                                    date: "Tue, 28th Jan'20  8:00 AM",
                                   ),
-                                  Padding(
-                                    padding: const EdgeInsets.all(5.0),
-                                    child: Text(
-                                      "seller: Seller name",
-                                      style: TextStyle(
-                                        fontSize: 16,
-                                      ),
-                                    ),
+                                  DeliveryStatus(
+                                    text: "Packed",
+                                    date: "Tue, 28th Jan'20  9:00 AM",
                                   ),
-                                  Padding(
-                                    padding: const EdgeInsets.all(8.0),
-                                    child: Text(
-                                      "₹ 32.00",
-                                      style: TextStyle(
-                                          color: Colors.black,
-                                          fontSize: 22,
-                                          fontWeight: FontWeight.bold),
-                                    ),
-                                  )
+                                  DeliveryStatus(
+                                    text: "Shipped",
+                                    date: "Tue, 28th Jan'20  9:30 AM",
+                                  ),
+                                  DeliveryStatus(
+                                    text: "Delivery",
+                                    date: "Expected\nTue, 28th Jan'20",
+                                  ),
                                 ],
                               ),
-                              CachedNetworkImage(
-                                imageUrl:
-                                    "https://www.periyarrice.com/images/slider2_pro1.png",
-                                height: 120,
-                                width: 120,
-                              ),
-                            ],
+                            ),
+                          ],
+                        ),
+                      ),
+                      Divider(
+                        color: Colors.grey,
+                        thickness: 1,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(6.0),
+                        child: SmoothStarRating(
+                          allowHalfRating: false,
+                          onRated: (v) {
+                            v = rating;
+                          },
+                          starCount: 5,
+                          size: 40.0,
+                          isReadOnly: false,
+                          color: kDesignColor,
+                          borderColor: Colors.grey,
+                          spacing: 10.0,
+                          rating: rating,
+                        ),
+                      ),
+                      Divider(
+                        color: Colors.grey,
+                        thickness: 1,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Text(
+                              "Need help?",
+                              style: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 17,
+                                  fontWeight: FontWeight.w500),
+                            ),
                           ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 10, right: 10),
+                child: Card(
+                  color: Colors.white,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(20)),
+                  ),
+                  elevation: 2,
+                  child: Padding(
+                    padding:
+                        const EdgeInsets.symmetric(horizontal: 25, vertical: 8),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(
+                          "Shopping Details",
+                          style: TextStyle(color: Colors.black54, fontSize: 14),
                         ),
                         Divider(
                           color: Colors.grey,
                           thickness: 1,
                         ),
                         Padding(
-                          padding: const EdgeInsets.symmetric(
-                              vertical: 5, horizontal: 25),
-                          child: Row(
-                            children: <Widget>[
-                              Container(
-                                height: 200,
-                                child: StepProgressIndicator(
-                                  totalSteps: 4,
-                                  direction: Axis.vertical,
-                                  currentStep: 3,
-                                  selectedColor: kDesignColor,
-                                  unselectedColor: Colors.grey,
-                                  size: 8,
-                                  padding: 1,
-                                  roundedEdges: Radius.circular(10),
-                                ),
-                              ),
-                              Padding(
-                                padding: const EdgeInsets.only(left: 15),
-                                child: Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: <Widget>[
-                                    DeliveryStatus(
-                                      text: "Ordered",
-                                      date: "Tue, 28th Jan'20  8:00 AM",
-                                    ),
-                                    DeliveryStatus(
-                                      text: "Packed",
-                                      date: "Tue, 28th Jan'20  9:00 AM",
-                                    ),
-                                    DeliveryStatus(
-                                      text: "Shipped",
-                                      date: "Tue, 28th Jan'20  9:30 AM",
-                                    ),
-                                    DeliveryStatus(
-                                      text: "Delivery",
-                                      date: "Expected\nTue, 28th Jan'20",
-                                    ),
-                                  ],
-                                ),
-                              ),
-                            ],
+                          padding: const EdgeInsets.symmetric(vertical: 2),
+                          child: Text(
+                            "Aleena Thomas",
+                            style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 18,
+                                fontWeight: FontWeight.w500),
                           ),
                         ),
-                        Divider(
-                          color: Colors.grey,
-                          thickness: 1,
-                        ),
+                        Text("Aleena house\nKannapuram\nKannur-670331")
                       ],
                     ),
                   ),
                 ),
-              ),
+              )
             ],
           ),
         ),
