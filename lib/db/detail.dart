e3bc8ed9cb6d80879fc75fb840d41250a555d80e
sanika2001 <porayilsanika@gmail.com>
2020-08-19 23:29:55 +0530

    order details

--- lib/db/detail.dart
+++ lib/db/detail.dart
@@ -36,15 +36,15 @@ class DatabaseProvider {
         "id INTEGER PRIMARY KEY, "
         "image TEXT, "
         "name TEXT, "
-        "price TEXT,"
+        "price TEXT"
         ")");
   }
 
   Future insertDB(Database database) async {
     await database.rawInsert(
         'INSERT INTO $detailTABLE($ID,$IMAGE,$NAME,$PRICE)VALUES (1, "https://www.periyarrice.com/images/slider2_pro1.png", "Periyar Rice","₹ 32.00"),'
-            '(2, "https://images-na.ssl-images-amazon.com/images/I/71CAb58u8TL._SL1313_.jpg","Aashirvaad Atta","₹ 84.00"),'
-            '(3,"https://www.periyarrice.com/images/rice_products2.png","Periyar Matta","₹ 40.00"),'
-            '(4,"https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQIXENIUsCLa8rU90YdPSm2rMDAkVUUhoxNDA&usqp=CAU","Periyar Rice1","₹ 50.00")');
+        '(2, "https://images-na.ssl-images-amazon.com/images/I/71CAb58u8TL._SL1313_.jpg","Aashirvaad Atta","₹ 84.00"),'
+        '(3,"https://www.periyarrice.com/images/rice_products2.png","Periyar Matta","₹ 40.00"),'
+        '(4,"https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQIXENIUsCLa8rU90YdPSm2rMDAkVUUhoxNDA&usqp=CAU","Periyar Rice1","₹ 50.00")');
   }
 }
